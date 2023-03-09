526f67fb1843bcb7bbb4b67e7e98e09757489ac8
Fadil Roni <muhammadfadilroni18@gmail.com>
2023-03-09 13:45:53 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	for i := 0; i < 5; i++ {
+		fmt.Println("Nilai i = ", i)
+	}
+
+	
+	for j := 0; j < 5; j++ {
+		fmt.Println("Nilai j = ", j)
+	}
+
+
+	var unicode = "САШАРВО"
+    for i, v := range unicode {
+        fmt.Printf("character %#U starts at byte position %d\n", v, i)
+    }
+
+	
+	for j := 6; j <= 10; j++ {
+		fmt.Println("Nilai j = ", j)
+	}
+
+
+}
